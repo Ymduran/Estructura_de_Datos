a60a49edf829819d78b4e82e70cc67d252496c2d
Duran Breceda <romeromatias273@gmail.com>
2024-10-14 15:16:48 -0600

    se agregó el archivo python y se metió dentro de la carpeta correcta

---
+++ Fundamentos_python/Tipos_de_datos_en_python_ejercicio.py
@@ -0,0 +1,60 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 10 octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     *")
+print(" * Ejercicio sobre los usos de los tipos básico de datos en Python. * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+
+# Ejercicios de tipos de datos.
+
+#Dato tipo string
+Dia = "Lunes"
+
+#Dato tipo entero
+Pasaje = 20
+
+#Datos tipo decimal
+Comida = 135.5
+Otros_Gastos = 34.5
+
+#Dato tipo Booleano
+Fue_mayor_mi_presupuesto = False
+
+#Para imprimir los letreros
+print("**Gastos diarios** ")
+print("Día: ", Dia)
+print("Pasaje:", Pasaje)
+print("Comida: ", Comida)
+print("Otros gastos: ", Otros_Gastos)
+print("¿Fue mayor mi presupuesto?: ", Fue_mayor_mi_presupuesto)
+
+#Modificación de datos
+
+# Modificación Dato tipo string
+Dia = "Martes"
+
+# Modificación Dato tipo entero
+Pasaje = 12
+
+# Modificación Datos tipo decimal
+Comida = 45.5
+Otros_Gastos = 4.5
+
+# Modificación Dato tipo Booleano
+Fue_mayor_mi_presupuesto = False
+
+#Para imprimir los letreros ahora con los datos modificados
+print(" ")
+print(" ")
+print("**Gastos diarios 2 ** ")
+print("Día: ", Dia)
+print("Pasaje:", Pasaje)
+print("Comida: ", Comida)
+print("Otros gastos: ", Otros_Gastos)
+print("¿Fue mayor mi presupuesto?: ", Fue_mayor_mi_presupuesto)
+
