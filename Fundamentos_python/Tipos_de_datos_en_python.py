bbfe3499c62f550bed2c88a3adbba7f589de1f16
Duran Breceda <romeromatias273@gmail.com>
2024-10-14 14:55:20 -0600

    se agregó el archivo python y se metió dentro de la carpeta correcta

---
+++ Fundamentos_python/Tipos_de_datos_en_python.py
@@ -0,0 +1,58 @@
+
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * ")
+print(" * Durán Breceda Lourdes Jamileth                         * ")
+print(" *                                                        * ")
+print(" * Fecha: 08 octubre del 2024                             * ")
+print(" *                                                        * ")
+print(" * Descripción:                                           *")
+print(" * Usos de los tipos básico de datos en Python.           * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * ")
+print(" ")
+print(" ")
+
+
+
+# Notas:
+"""
+En Python, no se requiere indicar el tipo de la variable en su declaración.
+Los valores básicos que pueden almacenar las variables son:
+
+- Int;  por ejemplo que edad = 1
+- Float; por ejemplo  pi = 3.1416
+- String (str); por ejemplo palabra = "vocal" 
+- Boolean. True o False (con inicial mayúscula); por ejemplo Es_Estudiante = True
+- None. Tipo especial que representa una ausencia de valor; Por ejemplo cero = None
+"""
+
+# Ejemplos de tipos de datos.
+
+# Número entero
+mi_variable_entera = -100
+print("Tipo de dato entero:",mi_variable_entera)
+
+# Número decimal
+mi_variable_decimal = 12.12
+print("Tipo de dato decimal:", mi_variable_decimal)
+
+# Cadena de texto
+mi_variable_texto_nombre = "Yamm"
+mi_variable_texto_apellido = 'Durán' # Con comillas simple también se imprime
+print("Cadena de texto: ", mi_variable_texto_nombre, mi_variable_texto_apellido)
+
+# Booleno
+mi_variable_booleana = True
+print('Tipo booleano:', mi_variable_booleana)
+
+# None
+mi_variable_none = None
+print("Tipo none:",mi_variable_none)
+
+
+# Uso de constantes.
+'''
+En Python, a diferencia de otros lenguajes de programación, no existe un tipo específico para definir constantes.
+Se utiliza una convención de colocar las variables en mayúsculas y no modificarlas.
+'''
+VARIABLE_CONSTANTE = 3.1416
+print("Ejemplo de convención de una constante:", VARIABLE_CONSTANTE)
+
