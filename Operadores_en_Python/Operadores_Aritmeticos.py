09b67360f95c71b27e4f3acd980afe459312fe73
Duran Breceda <romeromatias273@gmail.com>
2024-10-15 15:36:33 -0600

    se agregó el archivo python y se metió dentro de la carpeta correcta y solo se modificó la portada se terminó el ejhercicio

--- Operadores_en_Python/Operadores_Aritmeticos.py
+++ Operadores_en_Python/Operadores_Aritmeticos.py
@@ -8,3 +8,35 @@ print(" * Operadores Aritméticos                                           * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
+
+numero_entero1 = input("ingresa número entero 1: ")
+numero_entero2 = input("ingresa número entero 2: ")
+
+print("ud ingresó los múmeros ", numero_entero1 , " y ",numero_entero2)
+
+numero1 = int(numero_entero1)
+numero2 = int(numero_entero2)
+
+print("resultado de +: ", numero1 + numero2) #suma
+
+print("resultado de -: ", numero1 - numero2) #resta
+
+print("resultado de *: ", numero1 * numero2) # Multiplicación
+
+print("resultado de /: ", numero1 / numero2) #resultado de división en flotante (decimal)
+
+#print("resultado de / con dos decimales: ", numero1 / numero2 #resultado de división en flotante (decimal)
+
+print("resultado de //: ", numero1 // numero2) #resultado de división en entero
+
+print("resultado de %: ", numero1 % numero2) # Módulo de la divisón
+
+print("resultado de **: ", numero1 ** numero2) #eleva al primero a la potencia del segundo número
+
+
+
+
+
+
+
+
