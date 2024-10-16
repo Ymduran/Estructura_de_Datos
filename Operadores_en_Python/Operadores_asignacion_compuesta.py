e38e902c7c976e33dedd536c009fd00d161f2ffc
Duran Breceda <romeromatias273@gmail.com>
2024-10-16 15:14:07 -0600

    se agregó el archivo python y se metió dentro de la carpeta correcta y solo se modificó la portada
    se añadió portada y letreros

---
+++ Operadores_en_Python/Operadores_asignacion_compuesta.py
@@ -0,0 +1,17 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 14 octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Operadores Asignación compuesta                                  * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+numero_entero1 , numero_entero2= input("ingresa número entero 1:  ") , input("ingresa número entero 2:  ")
+#conversión a enteros
+numero1 = int(numero_entero1)
+numero2 = int(numero_entero2)
+print(f"ud ingresó los múmeros {numero1} y {numero2}")
+
