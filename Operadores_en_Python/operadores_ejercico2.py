29be088542498d8317ab7366b3cc6d0bd2d6f0ac
Duran Breceda <romeromatias273@gmail.com>
2024-10-17 15:56:53 -0600

    En este ejercicio tiene que mostrar un si cuando alguna de las respuestas sea verdadera

---
+++ Operadores_en_Python/operadores_ejercico2.py
@@ -0,0 +1,16 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 17 octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Operadores Operadores lódicos  ejercicio 2                       * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+respuesta1=input("Es profesor de la unsij si/no: ")
+respuesta1=respuesta1.lower()== "si"
+respuesta2=input("Es estudiante de la unsij? si/no: ")
+respuesta2=respuesta2.lower()== "si"
+print(f"forma parte de la comunidad de la unsij? {respuesta1 or respuesta2}")
