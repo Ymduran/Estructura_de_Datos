11b4efc8d509251f3945bdfd2b5741fbaabb32fd
Duran Breceda <romeromatias273@gmail.com>
2024-10-17 15:31:58 -0600

    En este programa se muestra el uso de los operadores aritméticos

--- Operadores_en_Python/Operadores_Lógicos.py
+++ Operadores_en_Python/Operadores_Lógicos.py
@@ -1,7 +1,7 @@
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" * Durán Breceda Lourdes Jamileth                                   * ")
 print(" *                                                                  * ")
-print(" * Fecha: 14 octubre del 2024                                       * ")
+print(" * Fecha: 17 octubre del 2024                                       * ")
 print(" *                                                                  * ")
 print(" * Descripción:                                                     * ")
 print(" * Operadores Operadores lódicos                                    * ")
@@ -9,6 +9,15 @@ print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-respuesta=input("ingrese si/no: ")
-variable_bool=bool(respuesta)
-print(f"su respuesta fue: {variable_bool}")
+variable_bool=input("1 ingrese si/no: ")
+variable_bool=variable_bool.lower()== "si"
+variable_bool2=input("2 ingrese si/no: ")
+variable_bool2=variable_bool2.lower()== "si"
+#lower convierte a minúscula  y despues se puede comparar dos cadenas
+print(f"su respuesta 1 fue: {variable_bool}") #si las cadenas son iguales entonce simprime true si no pues false
+print(f"su respuesta 2 fue: {variable_bool2}") #si las cadenas son iguales entonce simprime true si no pues false
+
+print(f"ambas respuestas fueron si? {variable_bool and variable_bool2}")
+print(f"alguna de las dos respuestas fueron si? {variable_bool or variable_bool2}")
+print(f"La primer respuesta fue no? {not variable_bool}")
+print(f"La segunda respuesta fue no? {not variable_bool2}")
