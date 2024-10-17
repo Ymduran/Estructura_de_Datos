fdb9ff115bb3d62cbea6b37db98bd2791ea7c4ff
Duran Breceda <romeromatias273@gmail.com>
2024-10-17 14:50:56 -0600

    se agregó el archivo python y se metió dentro de la carpeta correcta
    Se agregaron letreros
    Se agregaron letreros de los operadores lógicos

--- Operadores_en_Python/Operadores_relacionales.py
+++ Operadores_en_Python/Operadores_relacionales.py
@@ -10,15 +10,18 @@ print(" ")
 print(" ")
 
 numero_1 , numero_2= input("ingresa número decimal 1:  ") , input("ingresa número decimal 2:  ")
-#conversión a enteros
-numero1 = float(numero_1)
-numero2 = float(numero_2)
-print(f"ud ingresó los múmeros {numero1:.2f} y {numero2:.2f}")
+#conversión a flotantes
+numero1 = float(numero_1)  #después de leer un dato se debe convertir para realizar operaciones aritméticas con ellas
+numero2 = float(numero_2) #En este caso se convierte en un flotante o decimal porque serán los números a comparar
+print(f"ud ingresó los múmeros {numero1:.2f} y {numero2:.2f}") #nombre_variable:-2f para imprimir los primeros dos decimales
 
-print(f"¿ el primer número {numero1:.2f} es mayor a {numero2:.2f}?           {numero1>numero2}")
-print(f"¿ el primer número {numero1:.2f}  es mayor o igual a {numero2:.2f}?  {numero1>=numero2}")
-print(f"¿ el primer número {numero1:.2f} es menor a {numero2:.2f}?           {numero1<numero2}")
-print(f"¿ el primer número {numero1:.2f} es menor o igual a {numero2:.2f}?   {numero1<=numero2}")
-print(f"¿ Son iguales el número {numero1:.2f} al número {numero2:.2f}?       {numero1==numero2}")
-print(f"¿ Son diferentes los números {numero1:.2f} y {numero2:.2f}?          {numero1!=numero2}")
+
+#Las comparaciones se pueden realizar dentro de las llaves
+#el resultado de las comparaciones lógicas es una respuesta true or false
+print(f"¿ el primer número {numero1:.2f} es mayor a {numero2:.2f}?           {numero1>numero2}") #mayor que
+print(f"¿ el primer número {numero1:.2f}  es mayor o igual a {numero2:.2f}?  {numero1>=numero2}") #mayor o igual
+print(f"¿ el primer número {numero1:.2f} es menor a {numero2:.2f}?           {numero1<numero2}")#menor que
+print(f"¿ el primer número {numero1:.2f} es menor o igual a {numero2:.2f}?   {numero1<=numero2}")#menor o igual
+print(f"¿ Son iguales el número {numero1:.2f} al número {numero2:.2f}?       {numero1==numero2}")#igual
+print(f"¿ Son diferentes los números {numero1:.2f} y {numero2:.2f}?          {numero1!=numero2}")#diferente de
 
