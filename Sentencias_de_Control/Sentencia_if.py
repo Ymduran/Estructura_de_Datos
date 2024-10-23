1cfafe3302cce1a270b65e85339094cdc1f4577d
Duran Breceda <romeromatias273@gmail.com>
2024-10-23 15:56:11 -0600

    Se guardó

---
+++ Sentencias_de_Control/Sentencia_if.py
@@ -0,0 +1,17 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 17 octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Sentecncia if                                                    * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+'''
+if condición:
+    #Código a ejecutar si
+    #La condición es verdadera
+    
+En vez de llaves se usa tabulador
+'''
