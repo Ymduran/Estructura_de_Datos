ab87ec388fdaa8688643dc1b094d862f1651cba6
Duran Breceda <romeromatias273@gmail.com>
2024-10-23 15:56:52 -0600

    Se guardó

---
+++ Sentencias_de_Control/Cógigo_Determinar_número_es_par_o_impar.py
@@ -0,0 +1,16 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 17 octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Códogp que determina si un número es par o impar                 * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+numero = int(input("ingrese número: "))
+if (numero % 2) == 0:
+    print(f" el número {numero} es par")
+else:
+    print(f" el número {numero} es impar")
