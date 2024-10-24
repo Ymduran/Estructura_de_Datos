2209d630dbb34c2260139f6e395bd6d3f043123a
Duran Breceda <romeromatias273@gmail.com>
2024-10-24 14:19:02 -0600

    La sentencia "elif" es como "si no, sí" adicional. Se utiliza junto con if y else para crear estructuras de decisión más complejas (12)

--- Sentencias_de_Control/Sentencia_if_elif_else.py
+++ Sentencias_de_Control/Sentencia_if_elif_else.py
@@ -9,12 +9,19 @@ print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-edad = int(input("ingrese edad: "))
-if edad<=14 :
-    print(f" Ud tiene {edad}, pertenece a adolescentes ")
-elif (edad>=15) and (edad<=25) :
-    print(f" Ud tiene {edad}, pertenece a jóvenes ")
-elif (edad>=46) and (edad<=60):
-    print(f" Ud tiene {edad}, pertenece a adultos maduros ")
-elif edad>60:
-    print(f" Ud tiene {edad}, pertenece a adultos mayores ")
+'''
+if condición : 
+    #Código a ejecutar si
+elif condición2 :
+    #Código si ejecución 1 es falsa y condición dos es verdadera
+elif condición3:
+    #Código si la condición 1 y la condición 2 son falsas y la tercera es verdadera
+else :
+    #Código si ninguna condición es verdadera
+    
+'''
+
+'''
+    La sentencia "elif" es como "si no, sí" adicional. Se utiliza junto con if y else para crear estructuras de decisión más complejas
+'''
+
