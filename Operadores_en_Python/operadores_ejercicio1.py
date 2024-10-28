5ec778be3c73f871a3cb17c12c79d4427e26e154
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:45:17 -0600

    Operadores lógicos and, or, not, ejercio 1

--- Operadores_en_Python/operadores_ejercicio1.py
+++ Operadores_en_Python/operadores_ejercicio1.py
@@ -4,13 +4,16 @@ print(" *                                                                  * ")
 print(" * Fecha: 17 octubre del 2024                                       * ")
 print(" *                                                                  * ")
 print(" * Descripción:                                                     * ")
-print(" * Operadores Operadores lódicos  ejercicio                         * ")
+print(" * Operadores Operadores lógicos  ejercicio                         * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-compras=input("ingrese cantidad de sus compras ")
-compras=int(compras)
-meses_sin_intereses=input("las compras con a meses sin intereses? si/no: ")
-meses_sin_intereses=meses_sin_intereses.lower()== "si"
-print(f"aplica bonificacion? {(compras>5000) and meses_sin_intereses}")
+compras=input("ingrese cantidad de sus compras ") #Se lee la cantidad de compras por teclado
+compras=int(compras) #Se convierte a entero
+meses_sin_intereses=input("las compras con a meses sin intereses? si/no: ") #Se lee una respuesta
+meses_sin_intereses=meses_sin_intereses.lower()== "si" #Respuesta se convierte a minúsculas y posteriormente se compara con la cadena "si"
+print(f"aplica bonificacion? {(compras>5000) and meses_sin_intereses}") #Compara si la cantidad de compras es mayor que cinco mil y si la respuesta a la pregunta fue si
+                              #  true         and         true = true
+                              #  true         and         False =false
+                              #  False        and         False =false
