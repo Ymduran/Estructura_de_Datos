f405102be69112e21e93439b55a7a997d83abe8b
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:37:09 -0600

    Operadores lógico and, or, not

--- Operadores_en_Python/Operadores_Lógicos.py
+++ Operadores_en_Python/Operadores_Lógicos.py
@@ -4,12 +4,12 @@ print(" *                                                                  * ")
 print(" * Fecha: 17 octubre del 2024                                       * ")
 print(" *                                                                  * ")
 print(" * Descripción:                                                     * ")
-print(" * Operadores Operadores lódicos                                    * ")
+print(" * Operadores Operadores lógicos                                    * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-variable_bool=input("1 ingrese si/no: ")
+variable_bool=input("1 ingrese si/no: ") #Se lee por teclado si o no
 variable_bool=variable_bool.lower()== "si"
 variable_bool2=input("2 ingrese si/no: ")
 variable_bool2=variable_bool2.lower()== "si"
@@ -19,5 +19,5 @@ print(f"su respuesta 2 fue: {variable_bool2}") #si las cadenas son iguales enton
 
 print(f"ambas respuestas fueron si? {variable_bool and variable_bool2}")
 print(f"alguna de las dos respuestas fueron si? {variable_bool or variable_bool2}")
-print(f"La primer respuesta fue no? {not variable_bool}")
+print(f"La primer respuesta fue no? {not variable_bool}") #Negar la respuesta, es decir, "La primer respuesta fue no?" en caso de que sea true entonces lo ideal es imprimir que no fue no
 print(f"La segunda respuesta fue no? {not variable_bool2}")
