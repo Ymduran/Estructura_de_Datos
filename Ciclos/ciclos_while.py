02a7a2641c875784df07a2a068fe8f2b9f6fe677
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 15:22:25 -0600

    while sintaxis

---
+++ Ciclos/ciclos_while.py
@@ -0,0 +1,16 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 28  octubre del 2024                                       * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * ciclo while (mientras)                                          * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+
+
+'''
+while condición:
+--> #Código                                            #tabulacción 
+
+'''
