c37f5404d6984edb0a481bffadd1e57011def3f7
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:11:28 -0600

    Sentencia If y PROGRAMA PARA DETERMINAR SI ERES MEYOR DE EDAD

--- Sentencias_de_Control/Sentencia_if.py
+++ Sentencias_de_Control/Sentencia_if.py
@@ -15,3 +15,9 @@ if condición:
     
 En vez de llaves se usa tabulador, cuando se vuelve a la línea "principal" del código termina el código correspondiente al if 
 '''
+#PROGRAMA PARA DETERMINAR SI ERES MEYOR DE EDAD
+
+edad= int(input("ingrese su edad: ")) #Leer la edad por teclado
+if edad>=18: #sí edad es mayor o igual a 18 imprime lo siguiente
+        print("ud es mayor de 18 años")
+print("Mensaje fuera del if.") #Este mensaje se imprime siempre puesto que no está alineado con la instrucción que corresponde al if
