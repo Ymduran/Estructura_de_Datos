0403ec469d08e63b1fd64b66a442ab4ef855f265
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:28:29 -0600

    Sentencia If, elif y else y programa para determinar a qué grupo de edad perteneces

--- Sentencias_de_Control/Sentencia_if_elif_else.py
+++ Sentencias_de_Control/Sentencia_if_elif_else.py
@@ -14,7 +14,7 @@ if condición :
     #Código a ejecutar si
 elif condición2 :
     #Código si ejecución 1 es falsa y condición dos es verdadera
-elif condición3:
+elif condición3: 
     #Código si la condición 1 y la condición 2 son falsas y la tercera es verdadera
 else :
     #Código si ninguna condición es verdadera
@@ -22,6 +22,15 @@ else :
 '''
 
 '''
-    La sentencia "elif" es como "si no, sí" adicional. Se utiliza junto con if y else para crear estructuras de decisión más complejas
+    La sentencia "elif" es como "si no, sí" adicional. Se utiliza junto con if y else para crear estructuras de decisión más complejas, es como el if else de c
 '''
-
+#PROBRAMA PARA DETERMINAR A QUÉ GRUPO DE EDAD PERTENECES
+edad = int(input("ingrese edad: ")) # Se lee la edad por teclado
+if edad<=14 : #Condición 1. Si la edad es menor o igual que 14, arroja un resultado true/false
+    print(f" Ud tiene {edad}, pertenece a adolescentes ")  #Código a ejecutar si se cumple/ es verdadera la condición .
+elif (edad>=15) and (edad<=25) : #Condición 2. Sí edad está entre 15 y 25, arroja resultado true/false
+    print(f" Ud tiene {edad}, pertenece a jóvenes ") #Código a ejecutar sí se cumple/ es verdadera la condición 2.
+elif (edad>=46) and (edad<=60): #Condición 3. Sí la edad está entre 46 y 60, arroja un resultado de true/false
+    print(f" Ud tiene {edad}, pertenece a adultos maduros ") #Código a ejecutar si se cumple la condicón 3.
+elif edad>60:
+    print(f" Ud tiene {edad}, pertenece a adultos mayores ")
