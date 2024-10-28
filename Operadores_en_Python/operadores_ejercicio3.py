50db3cb5db32eefa9183241b2c4d8ba451f59ac3
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:52:23 -0600

    Operadores lógicos and, or, not, ejercio 3

--- Operadores_en_Python/operadores_ejercicio3.py
+++ Operadores_en_Python/operadores_ejercicio3.py
@@ -9,6 +9,6 @@ print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-numero=input("ingresa número: ")
-numero=int(numero)
-print(f"el número está entre 10 y  15? {(numero>=10) and (numero<=10)}")
+numero=input("ingresa número: ") #el número se lee por teclado
+numero=int(numero) #Lo que se leyó por teclado se convierte a entero
+print(f"el número está entre 10 y  15? {(numero>=10) and (numero<=10)}") #Evalúa si el número se encuentra entre número 10 y 15 con valores booleanos y los operadores lógicos; true and true = true
