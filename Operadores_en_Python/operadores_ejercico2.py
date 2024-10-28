4c750bcb7fae5844d7bf550c5478fb8a6eb5281d
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:50:27 -0600

    Operadores lógicos and, or, not, ejercio 2

--- Operadores_en_Python/operadores_ejercico2.py
+++ Operadores_en_Python/operadores_ejercico2.py
@@ -4,13 +4,17 @@ print(" *                                                                  * ")
 print(" * Fecha: 17 octubre del 2024                                       * ")
 print(" *                                                                  * ")
 print(" * Descripción:                                                     * ")
-print(" * Operadores Operadores lódicos  ejercicio 2                       * ")
+print(" * Operadores Operadores logicos  ejercicio 2                       * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
 
-respuesta1=input("Es profesor de la unsij si/no: ")
-respuesta1=respuesta1.lower()== "si"
-respuesta2=input("Es estudiante de la unsij? si/no: ")
-respuesta2=respuesta2.lower()== "si"
-print(f"forma parte de la comunidad de la unsij? {respuesta1 or respuesta2}")
+respuesta1=input("Es profesor de la unsij si/no: ") #Se lee por teclado la primera respuesta si/no
+respuesta1=respuesta1.lower()== "si" #Se convierte a minúsculas y se compara con la cadena si, esto dará un resultado booleano true/False
+respuesta2=input("Es estudiante de la unsij? si/no: ") #Se lee por teclado la segunda respuesta si/no
+respuesta2=respuesta2.lower()== "si"#Se convierte a minúsculas y se compara con la cadena si, esto dará un resultado booleano true/False
+print(f"forma parte de la comunidad de la unsij? {respuesta1 or respuesta2}") #Cuando alguna de las dos respuesatas sea correcta
+                                                #Algunos casos son:
+                                                # true or true = true
+                                                # true or false = true
+                                                # False or false = False
