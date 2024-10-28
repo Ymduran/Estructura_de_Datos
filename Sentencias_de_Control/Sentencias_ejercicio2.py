b5f55fe981f1c66e42873f9ce4d792d3d53fd087
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 17:16:47 -0600

    Sentencia_ejercicio2

--- Sentencias_de_Control/Sentencias_ejercicio2.py
+++ Sentencias_de_Control/Sentencias_ejercicio2.py
@@ -21,14 +21,14 @@ b) Utilice la lógica adecuada para determinar la estación del año de acuerdo 
 c) Muestre la estación correspondiente en consola.
 '''
 
-numero_de_mes=int(input("ingrese el número que respresente al mes: "))
-if numero_de_mes>=3 and numero_de_mes>=5:
+numero_de_mes=int(input("ingrese el número que respresente al mes: ")) #Se lee por tecladd el número de mes
+if numero_de_mes>=3 and numero_de_mes>=5: #1 condición: sí los meses se encuentran entre el tercer y quinto mes: true/false
     print("La estación es: Primavera")
-elif numero_de_mes>=6 and numero_de_mes<=8:
+elif numero_de_mes>=6 and numero_de_mes<=8: #2 condición: sí los meses se encuentran entre el sexto y octavo mes: true/false
     print("La estación es: Verano")
-elif numero_de_mes>=9 and numero_de_mes<=11:
+elif numero_de_mes>=9 and numero_de_mes<=11: #3 condición: sí los meses se encuentran entre el noveno y décimo primer mes: true/false
     print("La estación es: Otoño")
-elif numero_de_mes==12 or numero_de_mes==1 or numero_de_mes==2:
+elif numero_de_mes==12 or numero_de_mes==1 or numero_de_mes==2: #4 condición: si es diciembre, o enero o febrero
     print("La estación es: Invierno")
-else:
+else: #sí no se cumple ninguna de las condiciones anteriores
     print("Mes incorrecto")
