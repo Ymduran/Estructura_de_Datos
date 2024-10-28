b0c1acc0feac0540357a62430bed5cd89b5790a4
Duran Breceda <romeromatias273@gmail.com>
2024-10-28 16:20:54 -0600

    Sentencia If y else y programa que determina sí un número es par o impar

--- Sentencias_de_Control/Sentencia_if_Else.py
+++ Sentencias_de_Control/Sentencia_if_Else.py
@@ -14,9 +14,19 @@ if condición1:
     #Código a ejecutar sí
 else: 
     #Código si la condición1 no es verdadera
+    
+'''
+#Están agrupados por el tabulador, cuando se ocupa la siguiente línea sin tabular, entonces termina el código de la sentencia anterior
+'''
+Para decidir si se cumple una condición o no, sí no, ejecutar algo en particular
 '''
 
-'''
-Útil en un programa para decidir si una persona es mayor de edad o no
-'''
+#PROGRAMA PARA DETERMINAR SÍ UN NÚMERO ES PAR O IMPAR
+numero = int(input("ingrese número: ")) #Se lee el número por teclado
+if (numero % 2) == 0: # "%" = módulo, es decir el sobrante, la condición establece que si el sobrante de la división entre 2 es 0 entonces es par
+    #Un número par es todo aquel número múltiplo de 2
+    print(f" el número {numero} es par")
+else: #De no cumplirse la sentencia anterior, entonces se jecuta el siguiente código
+    print(f" el número {numero} es impar")
+#En este ejercicio tiene que mostrar un si cuando ambas respuestas sea verdadera.
 
