f31b32c050f396310365bd75160d42902df0d361
Duran Breceda <romeromatias273@gmail.com>
2024-10-29 15:59:57 -0600

    Operadores Asignación compuesta

--- Operadores_en_Python/Operadores_asignacion_compuesta.py
+++ Operadores_en_Python/Operadores_asignacion_compuesta.py
@@ -13,7 +13,7 @@ numero_entero1 , numero_entero2= input("ingresa número entero 1:  ") , input("i
 #conversión a enteros
 numero1 = int(numero_entero1)
 numero2 = int(numero_entero2)
-print(f"ud ingresó los múmeros {numero1} y {numero2}")
+print(f"ud ingresó los múmeros {numero1} y {numero2}")#muestra números que se ingresaron
 
 numero1 += 3
 print(f"numero1 += 3: {numero1}")
@@ -27,7 +27,7 @@ numero_entero1 , numero_entero2= input("ingresa número entero 1:  ") , input("i
 numero1 = int(numero_entero1)
 numero2 = int(numero_entero2)
 print(f"ud ingresó los múmeros {numero1} y {numero2}")
-
+#en python en vez de escribir " numero1 = numero1 + número2 " se puede escribir "numero1+=numero2"
 numero1 += numero2
 numero1 *= numero1
 numero1 -= numero2
