d2131dae59a889b851958b9eda056f6b6e411387
Duran Breceda <romeromatias273@gmail.com>
2024-11-13 15:05:25 -0600

    Este programa es para administrar y visualizar mis favoritos de youtube

--- Funciones_y_colecciones/Listas_ejercicios.py
+++ Funciones_y_colecciones/Listas_ejercicios.py
@@ -12,6 +12,7 @@ print(" ")
 '''
 '''
 
+# ESTA FUNCIÓN SOLO IMPRIME EL MENÚ, ASÍ QUE NO RECIBE NADA
 def menu ():
     print(" Favoritos; Videos de youtube")
     print(" ** Menú: ** ")
@@ -23,10 +24,11 @@ def menu ():
     print("[6].- Eliminar videos")
     print("[7].- Salir")
 
+# Definción de la lista
 nombre_video = [ ]
 
 
-
+# FUNCIÓN PARA HACER TODAS LAS OPERACIONES CON LAS LISTAS Y QUE RETORNA LA LISTA
 def lista_a_mostrar(opcion):
     if opcion == 1:
         len(nombre_video)  # ver lista por añadidos
@@ -48,7 +50,7 @@ def lista_a_mostrar(opcion):
 
     return nombre_video
 
-#FUNCI
+#FUNCIÓN PARA MOSTRAR LA LISTA DE VIDEOS, ESTA FUNCIÓN SOLO IMPRIME Y RECIBE LA LISTA DE LA FUNCIÓN "LISTA_A_MOSTRAR"
 def mostrar_lista (nombre_video):
     print(nombre_video, end = " ")
     print(" ")
@@ -58,14 +60,14 @@ def mostrar_lista (nombre_video):
 
 
 flag = 0
-while flag == 0:
+while flag == 0: # Hacer mientras que bandera sea igual a cero
     menu()
     opcion = int(input("Elige una opción: "))
     lista_a_mostrar(opcion)
     if opcion == 7:
         print("Saliendo...")
-        flag = 1
-    else:
+        flag = 1 #Si la opción es igual a 7 = Salir entonces la bandera cambia de valor y se rompe el ciclo
+    else: #Si la opción es otra que no sea 7, entonces muestra la lista y continúa el ciclo
         mostrar_lista(nombre_video)
 
 
