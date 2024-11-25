f364f40a3ede8603f0e1393df6f5f03ea3bda828
Duran Breceda <romeromatias273@gmail.com>
2024-11-25 16:34:46 -0600

    Programa añadido

--- Ciclos/Ciclos_ejercicio3_calculadora.py
+++ Ciclos/Ciclos_ejercicio3_calculadora.py
@@ -7,19 +7,14 @@ print(" * Descripción:                                                     * ")
 print(" * ciclos ejercicio número 3. calculadora                           * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
-
 print("  ")
-
-
-
 '''
 '''
-
 print(" ** CALCULADORA BÁSICA ** ")
 
 opcion = 1 #Se iguala a 1 para que pueda entrar al ciclo pero posteriormente se cambiará a la opción que se ingrese por teclado
 
-while opcion !=0: #Mientras que la opción sea diferente de cero seguirá mostrando el menú
+while opcion !=0: #Mientras que la opción sea diferente de cero seguirá mostrando el menú, al presionar cero será equivalente a la opción "salir"
     print(" ** Menú: ** ")
     print("[0].- Salir")
     print("[1].- Suma")
@@ -31,17 +26,22 @@ while opcion !=0: #Mientras que la opción sea diferente de cero seguirá mostra
 
     opcion = int(input("ingresa una opción: ")) #es necesario convertirlo a emtero porque a pesar de ya haber definido el tipo de datos en la línea 17, las variables en python son dinámicas, lo que significa que en diferentes puntos del programa puede tomar distintos tipos de valor
 
+
+
+
+
+
     if opcion==1:#suma
-        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: "))
-        print(f"{numero1} + {numero2} = {(numero1 + numero2):.2f}")
+        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: ")) #Lee los datos por teclado
+        print(f"{numero1} + {numero2} = {(numero1 + numero2):.2f}") #Hace la suma de los números en el print
         print("  ")
     elif opcion==2:#resta
-        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: "))
-        print(f"{numero1} - {numero2} = {(numero1 - numero2):.2f}")
+        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: ")) #Lee los datos por teclado
+        print(f"{numero1} - {numero2} = {(numero1 - numero2):.2f}") #Hace la resta de los números en el print
         print("  ")
     elif opcion==3:#multiplicación
-        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: "))
-        print(f"{numero1} * {numero2} = {(numero1 * numero2):.2f}")
+        numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: ")) #Lee los datos  por teclado
+        print(f"{numero1} * {numero2} = {(numero1 * numero2):.2f}") #Hace la multiplicación de los números desde el print y muestra los resultados
         print("  ")
     elif opcion==4:#división
         numero1, numero2 = float(input("Ingrese primer número: ")), float(input("Ingrese segundo número: "))
@@ -56,7 +56,7 @@ while opcion !=0: #Mientras que la opción sea diferente de cero seguirá mostra
         print(f"{numero1} ** {numero2} = {(numero1 ** numero2):.2f}")
         print("  ")
     elif opcion==0:
-        print("saliendo...")
+        print("saliendo...") #Justo antes de romper el cilo iimprime este letrero
     else:
         print("opción no válida")
 
