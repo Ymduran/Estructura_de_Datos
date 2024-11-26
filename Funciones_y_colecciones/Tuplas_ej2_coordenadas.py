78e733057c019d3a559578c8da2042d16fc01e79
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2024-11-25 23:10:43 -0600

    Update Tuplas_ej2_coordenadas.py

--- Funciones_y_colecciones/Tuplas_ej2_coordenadas.py
+++ Funciones_y_colecciones/Tuplas_ej2_coordenadas.py
@@ -1,1 +1,13 @@
+print("** EJEMPLOS CON COORDENADAS ** ")
+punto1 = (1,0)
+punto2 = (5,3)
 
+print(f"coordenadas en tuplas: {punto1} y {punto2}")
+#Desempaquetado de tuplas
+x1, y1 = punto1
+x2, y2 = punto2
+
+#Expresión de la recta: y=mx+b
+m = (y2-y1)/(x2-x1)
+b = y1 - (m*x1)
+print(f"el valor de la recta es: y = {m}x + {b}")
