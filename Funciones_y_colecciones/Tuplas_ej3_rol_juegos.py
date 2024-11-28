fdc09ff5aa572b9353627ae7b455bd5b60c9a554
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2024-11-28 15:23:51 -0600

    Update Tuplas_ej3_rol_juegos.py

--- Funciones_y_colecciones/Tuplas_ej3_rol_juegos.py
+++ Funciones_y_colecciones/Tuplas_ej3_rol_juegos.py
@@ -21,10 +21,7 @@ print("** Rol de juegos **")
 
 
 equipos = [] #Se crea la lista de equipos
-numero_equipos = int(input("Ingrese número (PAR) de equipos que jugarán:  "))
-if numero_equipos % 2 != 0:
-    print("Número impar :(")
-    exit()#Sale del programa
+numero_equipos = int(input("Ingrese número (PAR) de equipos que jugarán:  "))#Debe ser par porque los emparejamientos deben ser par
 
 
 
@@ -45,7 +42,7 @@ ultimo_equipo = numero_equipos-1
 for partido in range(numero_partidos):
     print(f"** Ronda {partido} **")
     ultimo_equipo = numero_equipos-1
-    for j in range(0, len(equipos)//2): #División entera
+    for j in range(0, len(equipos)//2): #División entera, la palabra "len" accede al número de elementos que hay en la lista, se divide entre dos porque solo hará la mitad de los equipos para asignarlos con la otra mitad
         if j == 0: #Cuando se esté en el caso 1, se asigna ese par. el primero con el último y se guarda como tupla en la lista de rol
             rol.append((pivote, equipos[ultimo_equipo]))
         else: #Para todos los demás casos solo se van guardando tal cual los pares en las tuplas
@@ -57,9 +54,6 @@ for partido in range(numero_partidos):
     # Imprimir los vs en pantalla
     for versus in rol:
         print(f"{versus[0]} vs {versus[1]}") #Imprime la lista
-
-
-
 #Lista[-1] refiere al último elemento de la lista
     equipos = [equipos[0]] +[equipos[-1]] + equipos[1:-1]   #el primero siempre se mantiene pues es el pivote, el siguiente es el último y el resto sigue igual
     rol = [] #Reincia la lista cada vez solo para imprimirlos
