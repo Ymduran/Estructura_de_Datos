105eb32a44a2ac8b406e93aed2d66f8ccd8f5758
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2024-12-08 20:46:52 -0600

    Create Ej3_lenguaje_hacker_l33t_speak.py

---
+++ Funciones_y_colecciones/Ej3_lenguaje_hacker_l33t_speak.py
@@ -0,0 +1,48 @@
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" * Durán Breceda Lourdes Jamileth                                   * ")
+print(" *                                                                  * ")
+print(" * Fecha: 10 de diciembre de 24                                     * ")
+print(" *                                                                  * ")
+print(" * Descripción:                                                     * ")
+print(" * Ej3_lenguaje_hacker_l33t_speak.py                                * ")
+print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
+print(" ")
+print(" ")
+
+'''
+Lenguaje básico:
+En el lenguaje básico se sustituye cada vocal por un número.
+La A se convierte en un 4, la E en un 3, la I en un 1 y la O en un cero.
+La letra U es una excepción, ya que no hay un número obvio, por lo que se sustituye por (_).
+
+Lenguaje intermedio:
+En el lenguaje intermedio también se sustituyen las consonantes por números o signos de puntuación.
+Por ejemplo, la B se convierte en I3, la C en [, la D en ), la L en 1.
+Revisa la "Leet speak alphabet" de la página anterior y toma la primera de las opciones para el lenguaje.
+Nota que no hay caracteres acentuados, por lo no se deben de convertir.
+Se debe mostrar el siguiente menú:
+  ***  Ejercicio 3. Lenguaje hacker (l33t sp34k).  ***
+1) Convertir un texto a lenguaje básico.
+2) Convertir un texto a lenguaje intermedio.
+0) Salir.
+Cualquier otro caso -> Opción no válida.
+Para ello:
+a) Utilice la lógica adecuada para mostrar las conversiones. No utilice funciones como bin() o hex().
+b) Utilice la lógica adecuada para convertir el texto al lenguaje hacker básico o intermedio, según sea el caso.
+c) Se debe convertir los caracteres sin importar si son mayúsculas o minúsculas, 
+sin modificar los caracteres que no se convirtieron. Ejemplos con el lenguaje básico: 
+hola -> h0l4, Hola -> H0l4, HOLA -> H0L4.
+'''
+
+
+#Conversión de letras en el nivel básico
+conversion_nivel_basico = {
+        a: 4,
+        A: 4,
+        e: 3,
+        E: 3,
+        i: 
+        
+    }
+    
+    
