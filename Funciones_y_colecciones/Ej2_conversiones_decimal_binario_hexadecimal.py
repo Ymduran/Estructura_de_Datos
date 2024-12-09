040f436774ab6422b647cfaa96fc387357847986
Durán Breceda Lourdes Jamileth <romeromatias273@gmail.com>
2024-12-09 09:52:35 -0600

    Update Ej2_conversiones_decimal_binario_hexadecimal.py

--- Funciones_y_colecciones/Ej2_conversiones_decimal_binario_hexadecimal.py
+++ Funciones_y_colecciones/Ej2_conversiones_decimal_binario_hexadecimal.py
@@ -39,7 +39,7 @@ def menu():
     print("[0].- Salir.")
 
 
-def decimal_a_binario_hexadecimal():
+def decimal_a_binario_hexadecimal(): #Función para convertir de decimal al binario
     numero_decimal = int(input("Ingrese un número decimal entero: "))
     numero_decimal_auxiliar = numero_decimal
     #Para convertir de decimal a binario
@@ -50,9 +50,9 @@ def decimal_a_binario_hexadecimal():
         lista_binario.append(digito_binario) #Agrega el 1 o 0, según corresponda a la lista_binario
         numero_decimal_auxiliar = numero_decimal_auxiliar // 2 #Ahora numero_decimal_auxiliar será la divisón entera del número decimal entre dos
         if numero_decimal_auxiliar == 0 or numero_decimal_auxiliar == 1: #Sí el resultado de la división entera del numero decimal entre dos es 1 o 0, significa que debe terminar el ciclo
-            lista_binario.append(numero_decimal_auxiliar)
-            flag_decimal_binario = 1
-    print(f"{numero_decimal} en binario es: ")
+            lista_binario.append(numero_decimal_auxiliar) #Añade a la lista el nuevo dígito, también este caso significa que es el último caso
+            flag_decimal_binario = 1 #Cambia la bandera y rompe el ciclo
+    print(f"{numero_decimal} en binario es: ") 
     #Invertir lista
     for i in range(1,len(lista_binario)+1):
         print(f"{lista_binario[i*-1]}", end="") #imprime la lista desde -1 hasta -n, donde -n es el número total de elementos de la lista, recordando que el índice -1 refiere al último elemento de la lista
@@ -60,17 +60,17 @@ def decimal_a_binario_hexadecimal():
     #Para convertir de decimal a hexadecimal
     print("\n")
     numero_decimal_auxiliar = numero_decimal #Reinicio el número decimal auxiliar
-    flag_decimal_hexadecimal = 0
+    flag_decimal_hexadecimal = 0 #Bandera para controlar el ciclo while que convierte a hexadecimal
     lista_hexadecimal = []
-    lista_letras_hexadecimal = ["A","B","C","D","E","F"]
-    while flag_decimal_hexadecimal == 0:
-        digito_hexadecimal = numero_decimal_auxiliar % 16
-        if digito_hexadecimal >= 10 and digito_hexadecimal <= 15:
-            digito_hexadecimal = lista_letras_hexadecimal[digito_hexadecimal-10]
-        lista_hexadecimal.append(digito_hexadecimal)
-        numero_decimal_auxiliar = numero_decimal_auxiliar // 16
-        if numero_decimal_auxiliar == 0 or numero_decimal_auxiliar == 1:
-            lista_hexadecimal.append(numero_decimal_auxiliar)
+    lista_letras_hexadecimal = ["A","B","C","D","E","F"] #Lista de los caracteres del 10 al 15 en hexadecimal
+    while flag_decimal_hexadecimal == 0: 
+        digito_hexadecimal = numero_decimal_auxiliar % 16 #el módulo de la división sobre 15 se asigna al dígito hexadecimal
+        if digito_hexadecimal >= 10 and digito_hexadecimal <= 15: #Pero si el dígito hexadecimal se encuentra entre el rango de 10 a 15, entonces se reemplaza por una letra
+            digito_hexadecimal = lista_letras_hexadecimal[digito_hexadecimal-10] #Según la letra que le corresponda de la lista_letas_hexadecimal
+        lista_hexadecimal.append(digito_hexadecimal) #Se añade a la lista de hexadecimal
+        numero_decimal_auxiliar = numero_decimal_auxiliar // 16 #Y ahora numero_decimal_auxiliar vale ese mismo número // 1(división entera)
+        if numero_decimal_auxiliar == 0 or numero_decimal_auxiliar == 1: #Significa que ya terminó de dividir
+            lista_hexadecimal.append(numero_decimal_auxiliar) #Y añade el último a la lista_hexadecimal
             flag_decimal_hexadecimal = 1
         
     print(f"{numero_decimal} en hexadecimal es: ")
@@ -117,7 +117,7 @@ def hexadecimal_a_decimal_binario():
     lista_digitos_hexadecimal = [un_digito for un_digito in numero_hexadecimal]
     lista_digitos_hexadecimal_auxiliar = lista_digitos_hexadecimal
 
-
+  #Dependiendo del valor literal que haya en la cadena se convierte a número
     for i in range(len(lista_digitos_hexadecimal)):
         if lista_digitos_hexadecimal[i] == "A":
             lista_digitos_hexadecimal_auxiliar[i] = 10
@@ -131,10 +131,10 @@ def hexadecimal_a_decimal_binario():
             lista_digitos_hexadecimal_auxiliar[i] = 14
         elif lista_digitos_hexadecimal[i] == "F":
             lista_digitos_hexadecimal_auxiliar[i] = 15
-        else:
+        else: #Sí no se trata de ninguna letra, entonces se convierte a entero directamente
             lista_digitos_hexadecimal_auxiliar[i] = int(lista_digitos_hexadecimal_auxiliar[i])
 
-    numero_decimal = 0
+    numero_decimal = 0 3esta será la sumatoria de los dígitos con un 1
     exponente = 0
     digito_invertido = -1
     for i in lista_digitos_hexadecimal_auxiliar:
@@ -162,7 +162,7 @@ def hexadecimal_a_decimal_binario():
               end="")  # imprime la lista desde -1 hasta -n, donde -n es el número total de elementos de la lista, recordando que el índice -1 refiere al último elemento de la lista
 
 
-def conversion(option):
+def conversion(option): #según la opción que haya ingresado el usuario manda a llamar la función correspondiente
     if option == 1:
         decimal_a_binario_hexadecimal()
     elif option == 2:
