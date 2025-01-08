99507f5a8ee6b9bbf254d17933be3b445ceb7b72
Duran Breceda <romeromatias273@gmail.com>
2025-01-08 16:23:00 -0600

    Programa añadido

--- Buenas_practicas/Especificar_datos_en_funciones.py
+++ Buenas_practicas/Especificar_datos_en_funciones.py
@@ -8,9 +8,8 @@ print(" * Especificar datos en funciones                                   * ")
 print(" * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * ")
 print(" ")
 print(" ")
-'''
 
-'''
+
 def menu():
     print("[1].- Convertir a entero")
     print("[2].- Convertir a flotante")
@@ -19,35 +18,42 @@ def menu():
     return opcion
 
 
-def cadena_a_entero(cadena:str) -> int | None: #Esta parte es para especificar el tipo de dato que recibo y el que retorno
+def cadena_a_entero(cadena: str) -> int | None:
     numero_guiones = cadena.count("-")
     revisar_cadena = cadena.lstrip("-")
-    if revisar_cadena.isNumeric() and numero_guiones in (0,1):
+    if revisar_cadena.isnumeric() and numero_guiones in (0, 1):
         return int(cadena)
     else:
         return None
 
 
-def cadena_a_flotante(cadena:str) -> float | None:
+def cadena_a_flotante(cadena: str) -> float | None:
     numero_puntos = cadena.count(".")
-    revisar_cadena = cadena.replace("")
-    if revisar_cadena.isNumeric() and numero_puntos in (0,1):
+    revisar_cadena = cadena.replace(".", "").lstrip("-")
+    if revisar_cadena.isnumeric() and numero_puntos in (0, 1):
         return float(cadena)
     else:
         return None
 
-#Código nivel de módulo
+
+# Código a nivel de módulo
 opcion = menu()
-while opcion != 0:
-    if opcion == 1:
+while int(opcion) != 0:
+    if int(opcion) == 1:
         num_cadena = input("Ingresa número a convertir: ")
         numero = cadena_a_entero(num_cadena)
-    elif opcion == 2:
+    elif int(opcion) == 2:
         num_cadena = input("Ingresa el número a convertir: ")
-        numero = cadena_a_entero(num_cadena)
+        numero = cadena_a_flotante(num_cadena)
+    else:
+        print("Opción no válida.")
+        opcion = menu()
+        continue
+
     while numero is None:
         num_cadena = input("Opción no válida. Intente nuevamente: ")
-        numero = cadena_a_entero(num_cadena)
-    print(f"El número {numero} es de tipo {type(numero)}")
+        numero = cadena_a_entero(num_cadena) if int(opcion) == 1 else cadena_a_flotante(num_cadena)
 
+    print(f"El número {numero} es de tipo {type(numero)}")
+    opcion = menu()
 
